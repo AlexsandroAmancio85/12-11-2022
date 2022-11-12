1daefa10d32c7e0263f0a8eae37a41f6dd07aa4c
AlexsandroAmancio85 <alexsandroamancio85@gmail.com>
2022-11-12 15:14:23 -0300

    estrutura de laço

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+# for i in range(1, 11, 1):
+#     print(i)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# for i in range(1, 11):
+#     print(i)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for i in range(10):
+    print(i)
+else:
+    print("laço finalizado")
